5d084a9903b24b2e4f45d7264516b8976184671a
James D <jrdelmare@gmail.com>
2019-05-11 14:12:00 -0700

    finised exam

--- src/app/test-score/test-score.component.ts
+++ src/app/test-score/test-score.component.ts
@@ -4,7 +4,14 @@ import { ActivatedRoute } from '@angular/router';
 import { Router } from '@angular/router';
 import { ToastService } from '../toast/toast.service';
 
-
+export interface ITest {
+  id?: number;
+  testName: string;
+  pointsPossible: number;
+  pointsReceived: number;
+  percentage: number;
+  grade: number;
+}
 @Component({
   selector: 'app-test-score',
   templateUrl: './test-score.component.html',
@@ -12,7 +19,7 @@ import { ToastService } from '../toast/toast.service';
 })
 export class TestScoreComponent implements OnInit {
 
-  tests: Array<any> = [];
+  tests: Array<ITest> = [];
   constructor(
     private http: Http,
     private activatedRoute: ActivatedRoute,
@@ -24,4 +31,59 @@ export class TestScoreComponent implements OnInit {
 
   }
 
+  addTest() {
+    const test: ITest = {
+      id: null,
+      testName: null,
+      pointsPossible: null,
+      pointsReceived: null,
+      percentage: null,
+      grade: null,
+    };
+    this.tests.unshift(test);
+      this.toastService.showToast('success', 5000, 'Test Added.');
+  }
+
+  deleteTest(index: number) {
+    this.tests.splice(index, 1);
+      this.toastService.showToast('success', 5000, 'Test was Deleted.');
+  }
+  saveTest() {
+    localStorage.setItem('tests', JSON.stringify(this.tests));
+      this.toastService.showToast('success', 5000, 'Success: Test Saved.');
+  }
+
+  async loadTestsFromJson() {
+    const tests = await this.http.get('assets/tests.json').toPromise();
+    return tests.json();
+  }
+
+  computeGrade() {
+    console.log('from finalize...');
+    const data = this.calculate();
+    this.router.navigate(['home', data]);
+  }
+
+  calculate() {
+    let grade = 0;
+    for (let i = 0; i < this.tests.length; i++) {
+      // console.log('i --->', i, "this.contacts[i]", this.contacts [i]);
+      grade += this.tests[i].grade;
+      // console.log('owed--->', owed);
+    }
+    return {
+      numberOfTests: this.tests.length,
+      pointsPossible: grade,
+      pointsEarned: grade * .10,
+      Precent: grade + (grade * .10)
+    };
+
+  }
+  search(params: string) {
+    console.log('from search ... params', params);
+
+    this.tests = this.tests.filter((test: ITest) => {
+      return test.testName.toLowerCase() === params.toLowerCase();
+    });
+  }
 }
